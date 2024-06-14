106e26af45b58b65fc388597840790c08f74f9b3
AlanaSR <svetlanka.shkola@gmail.com>
2024-06-15 02:12:54 +0500

    did 3 exercises

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Привет, куратор!");
+
+        System.out.println("Задание 1");
+        byte q = 1;
+        short w = 2;
+        int e = 4;
+        long r = 8L;
+        float t = 4.2f;
+        double y = 8.2;
+        System.out.println("Значение переменной q с типом byte равно "+q);
+        System.out.println("Значение переменной w с типом short равно "+w);
+        System.out.println("Значение переменной e с типом int равно "+e);
+        System.out.println("Значение переменной r с типом long равно "+r);
+        System.out.println("Значение переменной t с типом float равно "+t);
+        System.out.println("Значение переменной y с типом double равно "+y);
+
+        System.out.println("Задание 2");
+        float a = 27.12f;
+        long s = 987678965549L;
+        float d = 2.786f;
+        short f = 569;
+        short g = 159;
+        short h = 27897;
+        byte j = 67;
+
+        System.out.println("Задание 3");
+        byte lP = 23;
+        byte aS = 27;
+        byte eA = 30;
+        short allPaper = 480;
+        int forOnePupil = allPaper/(lP+aS+eA);
+        System.out.println("На каждого ученика рассчитано " + forOnePupil + " листов бумаги.");
+
+        System.out.println("Задание 4");
     }
 }
